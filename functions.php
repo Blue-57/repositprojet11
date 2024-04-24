62f61646e6f23c69623cc4f9650e878610dd5545
Blue <alexpaquin572@outlook.fr>
2024-04-24 10:48:52 +0200

    ajout post + taxonomie

--- functions.php
+++ functions.php
@@ -33,3 +33,4 @@ function theme_enqueue_styles()
     wp_enqueue_script('custom-script-contact', get_stylesheet_directory_uri() . '/assets/js/script-contact.js', array('jquery'), '1.0.0', true);
 }
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+
--- single-photos.php
+++ single-photos.php
@@ -1,1 +1,78 @@
-text
+<?php get_header();
+?>
+
+
+
+
+
+
+
+<div class="page-photo bloc-page">
+
+    <?php if (have_posts()):
+        while (have_posts()):
+            the_post(); ?>
+
+            <section class="photo-bloc colonnes">
+                <div class="photo-bloc__description colonne">
+                    <h1><?php the_title(); ?></h1>
+                    <p>Référence : <?php echo get_post_meta(get_the_ID(), 'reference', true); ?></p>
+                    <p>Catégorie : <?php echo strip_tags(get_the_term_list($post->ID, 'categories_photo')); ?></p>
+
+
+                    <p>Format : <?php echo get_field('format'); ?></p>
+                    <p>Type : <?php echo get_post_meta(get_the_ID(), 'type', true); ?></p>
+                    <p>Année : <?php echo get_the_date('Y'); ?></p>
+                </div>
+                <div class="photo-bloc__image colonne">
+                    <?php the_post_thumbnail(); ?>
+                </div>
+            </section>
+
+            <section class="interaction-photo colonnes">
+                <!-- Vos éléments d'interaction ici -->
+            </section>
+
+            <section class="recommandations">
+                <!-- Vos recommandations pour d'autres photos ici -->
+            </section>
+
+        <?php endwhile; endif; ?>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php get_footer(); ?>

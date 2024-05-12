3e2d3e9b9eeddd8a2798cb305d2fa0f8d51ae512
Blue <alexpaquin572@outlook.fr>
2024-05-12 19:15:58 +0200

    modification js images (erreur console +probleme affichage corriger

--- assets/js/script-contact.js
+++ assets/js/script-contact.js
@@ -38,8 +38,7 @@ jQuery(document).ready(function ($) {
 });
 
 
-
-// post page
+// post
 document.addEventListener('DOMContentLoaded', function () {
     const arrowGauche = document.querySelector('.fleche-gauche');
     const arrowDroite = document.querySelector('.fleche-droite');
@@ -47,41 +46,81 @@ document.addEventListener('DOMContentLoaded', function () {
     const nextImage = document.querySelector('.next-image');
     const navigationBlock = document.querySelector('.interaction-photo__navigation');
 
-    // Cacher les images par défaut
-    previousImage.style.opacity = '0';
-    nextImage.style.opacity = '0';
+    // Cacher les images par défaut en ajustant l'opacité si elles existent
+    if (previousImage) {
+        previousImage.style.opacity = '0';
+    }
+    if (nextImage) {
+        nextImage.style.opacity = '0';
+    }
 
     // Afficher les images lorsque la souris est à l'intérieur du bloc
     navigationBlock.addEventListener('mouseenter', function () {
-        previousImage.style.opacity = '1';
-        nextImage.style.opacity = '1';
+        if (previousImage) {
+            previousImage.style.opacity = '1';
+        }
+        if (nextImage) {
+            nextImage.style.opacity = '1';
+        }
     });
 
     // Cacher les images lorsque la souris quitte le bloc
     navigationBlock.addEventListener('mouseleave', function () {
-        previousImage.style.opacity = '0';
-        nextImage.style.opacity = '0';
-    });
-
-    // Afficher l'image précédente au survol de la flèche gauche
-    arrowGauche.addEventListener('mouseover', function () {
-        if (navigationBlock.contains(arrowGauche)) {
-            previousImage.style.opacity = '1';
+        if (previousImage) {
+            previousImage.style.opacity = '0';
+        }
+        if (nextImage) {
             nextImage.style.opacity = '0';
         }
     });
 
+
+    if (arrowGauche) {
+        arrowGauche.addEventListener('mouseover', function () {
+
+
+
+            arrowGauche.addEventListener('mouseover', function () {
+                if (navigationBlock.contains(arrowGauche)) {
+                    if (previousImage) {
+                        previousImage.style.opacity = '1';
+                    }
+                    if (nextImage) {
+                        nextImage.style.opacity = '0';
+                    }
+                } else if (navigationBlock.contains(arrowDroite) && previousImage && previousImage.src === "") {
+                    if (nextImage) {
+                        nextImage.style.opacity = '1';
+                    }
+                }
+            });
+        });
+    }
+
     // Afficher l'image suivante au survol de la flèche droite
-    arrowDroite.addEventListener('mouseover', function () {
-        if (navigationBlock.contains(arrowDroite)) {
-            previousImage.style.opacity = '0';
-            nextImage.style.opacity = '1';
-        }
-    });
+    if (arrowDroite) {
+        arrowDroite.addEventListener('mouseover', function () {
+
+
+            arrowDroite.addEventListener('mouseover', function () {
+                if (navigationBlock.contains(arrowDroite)) {
+                    if (previousImage) {
+                        previousImage.style.opacity = '0';
+                    }
+                    if (nextImage) {
+                        nextImage.style.opacity = '1';
+                    }
+                } else if (navigationBlock.contains(arrowGauche) && nextImage && nextImage.src === "") {
+                    if (previousImage) {
+                        previousImage.style.opacity = '1';
+                    }
+                }
+            });
+        });
+
+    }
+
 });
-
-
-
 
 
 

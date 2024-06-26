8dcb2a88b5cec547fa608d64d706c6a61b7402b3
Blue <alexpaquin572@outlook.fr>
2024-06-26 13:17:31 +0200

    finalisation reponsive

--- templates/lightbox.php
+++ templates/lightbox.php
@@ -7,9 +7,9 @@
             <p class="photo-reference"></p>
         </div>
         <img class="lightbox-close" src="<?php echo get_template_directory_uri(); ?>/assets/img/croix.png" alt="Fermer">
-        <img class="lightbox-prev"
-            src="<?php echo get_template_directory_uri(); ?>/assets/img/Navigation arrowsgauche.png" alt="Précédent">
-        <img class="lightbox-next"
-            src="<?php echo get_template_directory_uri(); ?>/assets/img/Navigation arrowsdroite.png" alt="Suivant">
+        <img class="lightbox-prev" src="<?php echo get_template_directory_uri(); ?>/assets/img/Line 7.png"
+            alt="Précédent">
+        <img class="lightbox-next" src="<?php echo get_template_directory_uri(); ?>/assets/img/Line 8.png"
+            alt="Suivant">
     </div>
 </div>
